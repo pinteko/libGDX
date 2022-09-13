8374f177087e23a19bfbd10b5bbf4e38c6ad2609
Kirill Korsuk <korsukkirill@gmail.com>
2022-09-13 09:25:05 +0300

    update contList and gamescreen, create WinScreen and GameScreenNextLevel

--- core/src/com/mygdx/game/screens/WinScreen.java
+++ core/src/com/mygdx/game/screens/WinScreen.java
@@ -9,36 +9,35 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Main;
 
-public class GameOverScreen implements Screen {
+public class WinScreen implements Screen {
     private Main game;
 
     private final SpriteBatch batch;
 
     private final Texture img;
-    private final Texture button_again;
+    private final Texture button_goNext;
     private final Texture button_exit;
-    private final Rectangle againRect;
+    private final Rectangle goNextRect;
     private final Rectangle exitRect;
 
     private final Music music;
 
     private ShapeRenderer shapeRenderer;
 
-    public GameOverScreen(Main main) {
+    public WinScreen(Main main) {
         this.game = main;
         batch = new SpriteBatch();
-        img = new Texture("GameO.jpg");
-        button_again = new Texture("tryAgain.jpg");
+        img = new Texture("win.jpg");
+        button_goNext = new Texture("next.png");
         button_exit = new Texture("exit.jpg");
-        againRect = new Rectangle(0, 0, button_again.getWidth(), button_again.getHeight());
+        goNextRect = new Rectangle(0, 0, button_goNext.getWidth(), button_goNext.getHeight());
         exitRect = new Rectangle(Gdx.graphics.getWidth() - button_exit.getWidth(),
                 0, button_exit.getWidth(), button_exit.getHeight());
         shapeRenderer = new ShapeRenderer();
-        music = Gdx.audio.newMusic(Gdx.files.internal("gameover.wav")); //написать имя музыки из папки assets
-        music.setLooping(true); //повторяемость
+        music = Gdx.audio.newMusic(Gdx.files.internal("winMusic.wav")); //написать имя музыки из папки assets
+        music.setLooping(false); //повторяемость
         music.setVolume(0.25f);
         music.play();
     }
@@ -51,18 +50,16 @@ public class GameOverScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(Color.BLUE);
-
         batch.begin();
         batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(button_again, 0, 0, button_again.getWidth(), button_again.getHeight());
+        batch.draw(button_goNext, 0, 0, button_goNext.getWidth(), button_goNext.getHeight());
         batch.draw(button_exit, Gdx.graphics.getWidth() - button_exit.getWidth(),
                 0, button_exit.getWidth(), button_exit.getHeight());
         batch.end();
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(Color.BLACK);
-        shapeRenderer.rect(againRect.x, againRect.y, againRect.width, againRect.height);
+        shapeRenderer.rect(goNextRect.x, goNextRect.y, goNextRect.width, goNextRect.height);
         shapeRenderer.rect(exitRect.x, exitRect.y, exitRect.width, exitRect.height);
         shapeRenderer.end();
 
@@ -70,9 +67,9 @@ public class GameOverScreen implements Screen {
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             int x = Gdx.input.getX();
             int y = Gdx.graphics.getHeight() - Gdx.input.getY();
-            if (againRect.contains(x, y)) {
+            if (goNextRect.contains(x, y)) {
                 dispose();
-                game.setScreen(new GameScreen(game));
+                game.setScreen(new GameScreenNextLevel(game));
             }
             if (exitRect.contains(x, y)) {
                 dispose();
@@ -80,33 +77,33 @@ public class GameOverScreen implements Screen {
         }
     }
 
-        @Override
-        public void resize(int width, int height) {
+    @Override
+    public void resize(int width, int height) {
 
-        }
+    }
 
-        @Override
-        public void pause() {
+    @Override
+    public void pause() {
 
-        }
+    }
 
-        @Override
-        public void resume() {
+    @Override
+    public void resume() {
 
-        }
+    }
 
-        @Override
-        public void hide() {
+    @Override
+    public void hide() {
 
-        }
+    }
 
-        @Override
-        public void dispose() {
-            batch.dispose();
-            img.dispose();
-            music.dispose();
-            shapeRenderer.dispose();
-            button_exit.dispose();
-            button_again.dispose();
-        }
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        music.dispose();
+        shapeRenderer.dispose();
+        button_exit.dispose();
+        button_goNext.dispose();
+    }
 }
